ec87cc8a9e8f31ed769d79aa2604b4733835ba8b
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-06 10:28:15 +0900

    retrying connection

--- rabbitmq.ts
+++ rabbitmq.ts
@@ -1,59 +1,54 @@
-import amqplib from 'amqplib'
-import dotenv from 'dotenv'
-import { io } from '.'
+import amqplib from "amqplib"
+import dotenv from "dotenv"
+import { io } from "."
 
 dotenv.config()
 
-const {
-    RABBITMQ_URL = 'amqp://localhost',
-    RABBITMQ_QUEUES = '',
-} = process.env
+const { RABBITMQ_URL = "amqp://localhost", RABBITMQ_QUEUES = "" } = process.env
 
-const queues = RABBITMQ_QUEUES.split(',')
-
+const queues = RABBITMQ_QUEUES.split(",")
 
 const messageCallback = (message: any) => {
-    const queue = message.fields.routingKey
-    const messageString = message.content.toString()
-    const messageJson = JSON.parse(messageString)
-    io.emit(queue, messageJson)
+  const queue = message.fields.routingKey
+  const messageString = message.content.toString()
+  const messageJson = JSON.parse(messageString)
+  io.emit(queue, messageJson)
 }
 
 const connectionErrorCallback = (error: any) => {
-    console.log(`[RabbitMQ] Connection lost`)
-    init()
+  console.log(`[RabbitMQ] Connection lost`)
+  init()
 }
 
 const channelErrorCallback = (error: any) => {
-    console.log(error)
-    init()
+  console.log(error)
+  init()
 }
 
 const init = async () => {
-
+  try {
+    console.log(`[RabbitMQ] Connecting to ${RABBITMQ_URL} ...`)
     const connection = await amqplib.connect(RABBITMQ_URL)
-    console.log('[RabbitMQ] connected')
+    console.log("[RabbitMQ] connected")
 
-    connection.on('error', connectionErrorCallback);
-
+    connection.on("error", connectionErrorCallback)
 
     const channel = await connection.createChannel()
-    console.log('[RabbitMQ] Channel created')
+    console.log("[RabbitMQ] Channel created")
 
-    channel.on('error', channelErrorCallback)
+    channel.on("error", channelErrorCallback)
 
     const consumeOptions = { noAck: false }
 
     for await (const queue of queues) {
-        // AssertQueue will create the queue if it does not exist
-        await channel.assertQueue(queue, { durable: false })
-        channel.consume(queue, messageCallback, consumeOptions)
+      // AssertQueue will create the queue if it does not exist
+      await channel.assertQueue(queue, { durable: false })
+      channel.consume(queue, messageCallback, consumeOptions)
     }
+  } catch (error) {
+    console.log("[RabbitMQ] Init failed, retrying in 5s...")
+    setTimeout(init, 5000)
+  }
 }
 
-
-export {
-    init,
-    RABBITMQ_URL as url,
-    queues as queues,
-}
+export { init, RABBITMQ_URL as url, queues as queues }
